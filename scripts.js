f28cef6cc263ab88c92c3a68b5f7d27d8b57542f
AHowardC <achsvgusaeng@yahoo.com>
2017-10-18 14:05:22 -0400

    the update.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,25 @@
+function findPlaces() {
+  var where = navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position.coords.latitude, position.coords.longitude);
+    var myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+    var map = new google.maps.Map(document.getElementById('map'),
+      {
+        zoom: 4,
+        center: myLatlng 
+      }
+    );
+
+    var y = new google.maps.places.PlacesService(map);
+    console.dir(y)
+  });
+
+}
+
 $(document).ready(()=>{ 
 
 
-var where = navigator.geolocation.getCurrentPosition(function(position) {
-console.log(position.coords.latitude, position.coords.longitude);
-});
 
-var map = new google.maps.Map(document.getElementById('map'),
-    {
-      zoom: 4,
-      center: myLatlng
-    }
-  );
 
-var y = new google.maps.places.PlacesService(map);
 
 
 
