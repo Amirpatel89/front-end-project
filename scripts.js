00c51b8bd40a89636805d718ac467ed97a1fdfbd
Amir Patel <31699548+Amirpatel89@users.noreply.github.com>
2017-10-23 11:05:21 -0400

    updated javascript

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@
   var restArray = [];
   var sorted;
   var placeDetails = [];
+  var globalThisRest = [];
+
 
 
 function getWhere(){
@@ -11,13 +13,13 @@ function getWhere(){
       var thisLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
       console.log(thisLocation);
       console.log(position);
-      var defaultLocation = {lat: 36.169941, lng: -115.139830};
+      // var defaultLocation = {lat: 36.169941, lng: -115.139830};
       // console.log(defaultLocation);
       initMap(thisLocation);
 
 
 
-
+      // initMap(thisLocation);
 
      
     
@@ -76,39 +78,45 @@ function callback(results, status) {
               counter = (counter + 1) % sortedArray.length
               var thisRest = (sortedArray[counter]);
               console.log(thisRest);
+              globalThisRest.push(thisRest);
+
             $('.name-of-restaurant').html(`${thisRest.name}`);
             $('.rating-of-restaurant').html(`Rating: ${thisRest.rating} Stars`);
 
 
-            });
-            // $('reset-button').click(function(){
+            // $('reset-button').click(function reset(){
             // sortedArray.length =0
             // counter = 0
             // var thisRest = (sortedArray[counter]);
             // console.log(thisRest);
             // $('.name-of-restaurant').html(`${thisRest.name}`);
             // $('.rating-of-restaurant').html(`Rating: ${thisRest.rating} Stars`);
-            //   // var photo = $('.photo-of-restaurant').prepend('<img id="thePhoto" src=""https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + "`${thisRest.reference}`" + "&key=AIzaSyCtqWrXYroKEJA-TxgqRjLZzGsTa4bsRLk"" />');
-            //   // console.log(photo);
-            // var service = new google.maps.places.PlacesService(map);
-            //     service.getDetails({placeId: `${thisRest.place_id}`}, function(place, status) {
-            //        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            //         console.log(place);
-            //         // placeDetails.push(place.photos[0]);
-            //         var photo = [];
-            //         photo.push(`${place.photos[counter]}`)
-            //         // $('.photo-of-restaurant').html(`${place.photos[0]}`);
-            //         $('.photo-of-restaurant').html(JSON.stringify(photo));
+              // var photo = $('.photo-of-restaurant').prepend('<img id="thePhoto" src=""https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + "`${thisRest.reference}`" + "&key=AIzaSyCtqWrXYroKEJA-TxgqRjLZzGsTa4bsRLk"" />');
+              // console.log(photo);
+            var service = new google.maps.places.PlacesService(map);
+                service.getDetails({placeId: `${thisRest.place_id}`}, function(place, status) {
+                   if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    console.log(place);
+                    placeDetails.push(`${place.photos[0]}`);
+                    var photo = [];
+                    photo.push(`${place.photos[0]}`)
+                    // $('.photo-of-restaurant').html(`${place.photos[0]}`);
+                    console.log(photo);
+                    console.log(placeDetails);
+                    $('.photo-of-restaurant').html(placeDetails);
+
+                    };
 
 
 
 
+            });
+
+        });
                 
+};
 
-
-            //       }
-            //     });
-            
+                 
 
 
                
@@ -117,21 +125,22 @@ function callback(results, status) {
 
 
             // });
-            // $('.go-back').click(function(){
-            //   counter = (counter - 1) % sortedArray.length
-            //   var thisRest = (sortedArray[counter])
-            //   console.log(thisRest);
-            //   $('.name-of-restaurant').html(`${thisRest.name}`);
-            //   $('.rating-of-restaurant').html(`Rating: ${thisRest.rating} Stars`);
-            //      // if(counter == 0){
-            //      //   counter == sortedArray.length-1
-            //      // };
+            // $('.go-back').click(function goBack(){
+            //   console.log("hlkjdsf");
+              // counter = (counter - 1) % sortedArray.length
+              // var thisRest = (sortedArray[counter])
+              // console.log(thisRest);
+              // $('.name-of-restaurant').html(`${thisRest.name}`);
+              // $('.rating-of-restaurant').html(`Rating: ${thisRest.rating} Stars`);
+                 // if(counter == 0){
+                 //   counter == sortedArray.length-1
+                 // };
                  
-            //      //  else {
-            //      //      counter=counter-1
-            //      //  };
+            //       else {
+            //           counter=counter-1
+            //       };
             // });
-}
+
 function createMarker(place) {
   var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
